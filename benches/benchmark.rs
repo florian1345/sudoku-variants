f98d58e7afa25b7b8166bc92eedd632d8054a4ef
florian1345 <florian1345@gmx.de>
2021-04-03 01:54:45 +0200

    Changed benchdata to json

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -7,9 +7,6 @@ use criterion::{
 };
 use criterion::measurement::WallTime;
 
-use toml::Value;
-use toml::value::Table;
-
 use sudoku_variants::{Sudoku, SudokuGrid};
 use sudoku_variants::constraint::{
     AdjacentConsecutiveConstraint,
@@ -30,6 +27,8 @@ use sudoku_variants::solver::strategy::{
     StrategicBacktrackingSolver,
     TupleStrategy
 };
+
+use serde::Deserialize;
 
 use std::fs;
 use std::time::Duration;
@@ -54,72 +53,12 @@ const DEFAULT_SAMPLE_SIZE: usize = 100;
 const CONSTRAINED_SAMPLE_SIZE: usize = 100;
 
 const BENCHDATA_DIR: &'static str = "benchdata/";
-const TASK_FILE_EXT: &'static str = ".toml";
+const TASK_FILE_EXT: &'static str = ".json";
 
+#[derive(Deserialize)]
 struct Task<C: Constraint + Clone> {
     puzzle: Sudoku<C>,
     solution: SudokuGrid
-}
-
-fn get_entry<'a>(table: &'a Table, name: &str) -> &'a Value {
-    table.get(name)
-        .unwrap_or_else(||
-            panic!("Expected {}, but was not present.", name))
-}
-
-fn get_array<'a>(table: &'a Table, name: &str) -> &'a Vec<Value> {
-    let value = get_entry(table, name);
-    value.as_array()
-        .unwrap_or_else(||
-            panic!("Expected {} as array, but was different type.", name))
-}
-
-fn get_string<'a>(table: &'a Table, name: &str) -> &'a str {
-    let value = get_entry(table, name);
-    value.as_str()
-        .unwrap_or_else(||
-            panic!("Expected {} as string, but was different type.", name))
-}
-
-impl<C: Constraint + Clone> Task<C> {
-    fn parse(value: &Value, constraint: C) -> Task<C> {
-        if let Value::Table(table) = value {
-            let puzzle_code = get_string(table, "puzzle");
-            let solution_code = get_string(table, "solution");
-            let puzzle = Sudoku::parse(puzzle_code, constraint).unwrap();
-            let solution = SudokuGrid::parse(solution_code).unwrap();
-
-            if !puzzle.is_valid_solution(&solution).unwrap() {
-                panic!("Invalid solution for task.");
-            }
-
-            Task {
-                puzzle,
-                solution
-            }
-        }
-        else {
-            panic!("Tried to parse task from non-table.");
-        }
-    }
-}
-
-fn parse_tasks<C: Constraint + Clone>(toml: String, constraint: C)
-        -> Vec<Task<C>> {
-    let root = toml.parse::<Value>()
-        .expect("Tried to parse task vector from invalid TOML.");
-    let root_table = root.as_table()
-        .expect("Root object is not a table.");
-    let array = get_array(root_table, "tasks");
-    array.iter()
-        .map(|value| Task::parse(value, constraint.clone()))
-        .collect()
-}
-
-fn parse_tasks_from_file<C: Constraint + Clone>(file: String, constraint: C)
-        -> Vec<Task<C>> {
-    let toml = fs::read_to_string(file).unwrap();
-    parse_tasks(toml, constraint)
 }
 
 fn solve_task<C: Constraint + Clone, S: Solver>(task: &Task<C>, solver: &S) {
@@ -134,9 +73,12 @@ fn solve_tasks<C: Constraint + Clone, S: Solver>(tasks: &Vec<Task<C>>,
     }
 }
 
-fn benchmark_solver_constraint<C: Constraint + Clone, S: Solver>(
-        group: &mut BenchmarkGroup<WallTime>, id: &str, sample_size: usize,
-        constraint: C, solver: &S) {
+fn benchmark_solver_constraint<C, S>(group: &mut BenchmarkGroup<WallTime>,
+    id: &str, sample_size: usize, solver: &S)
+where
+    for<'de> C: Constraint + Clone + Deserialize<'de>,
+    S: Solver
+{
     group.measurement_time(Duration::from_secs(MEASUREMENT_TIME_SECS));
     group.sample_size(sample_size);
     group.sampling_mode(SamplingMode::Flat);
@@ -144,34 +86,35 @@ fn benchmark_solver_constraint<C: Constraint + Clone, S: Solver>(
     let mut file = String::from(BENCHDATA_DIR);
     file.push_str(id);
     file.push_str(TASK_FILE_EXT);
-    let tasks: Vec<Task<C>> = parse_tasks_from_file(file, constraint);
+    let json = fs::read_to_string(file).unwrap();
+    let tasks: Vec<Task<C>> = serde_json::from_str(&json).unwrap();
 
     group.bench_function(id, |b| b.iter(|| solve_tasks(&tasks, solver)));
 }
+
+type DefaultAndDiagonalsConstraint =
+    CompositeConstraint<DefaultConstraint, DiagonalsConstraint>;
+type DefaultAndKnightsMoveConstraint =
+    CompositeConstraint<DefaultConstraint, KnightsMoveConstraint>;
+type DefaultAndKingsMoveConstraint =
+    CompositeConstraint<DefaultConstraint, KingsMoveConstraint>;
+type DefaultAndAdjacentConsecutiveConstraint =
+    CompositeConstraint<DefaultConstraint, AdjacentConsecutiveConstraint>;
 
 fn benchmark_solver<S: Solver>(c: &mut Criterion, group_name: &str,
         solver: S) {
     let mut group = c.benchmark_group(group_name);
 
-    benchmark_solver_constraint(&mut group, "default", DEFAULT_SAMPLE_SIZE,
-        DefaultConstraint, &solver);
-    benchmark_solver_constraint(&mut group, "diagonals",
-        CONSTRAINED_SAMPLE_SIZE,
-        CompositeConstraint::new(DefaultConstraint, DiagonalsConstraint),
-        &solver);
-    benchmark_solver_constraint(&mut group, "knights-move",
-        CONSTRAINED_SAMPLE_SIZE,
-        CompositeConstraint::new(DefaultConstraint, KnightsMoveConstraint),
-        &solver);
-    benchmark_solver_constraint(&mut group, "kings-move",
-        CONSTRAINED_SAMPLE_SIZE,
-        CompositeConstraint::new(DefaultConstraint, KingsMoveConstraint),
-        &solver);
-    benchmark_solver_constraint(&mut group, "adjacent-consecutive",
-        CONSTRAINED_SAMPLE_SIZE,
-        CompositeConstraint::new(DefaultConstraint,
-            AdjacentConsecutiveConstraint),
-        &solver);
+    benchmark_solver_constraint::<DefaultConstraint, _>(&mut group, "default",
+        DEFAULT_SAMPLE_SIZE, &solver);
+    benchmark_solver_constraint::<DefaultAndDiagonalsConstraint, _>(&mut group,
+        "diagonals", CONSTRAINED_SAMPLE_SIZE, &solver);
+    benchmark_solver_constraint::<DefaultAndKnightsMoveConstraint, _>(
+        &mut group, "knights-move", CONSTRAINED_SAMPLE_SIZE, &solver);
+    benchmark_solver_constraint::<DefaultAndKingsMoveConstraint, _>(&mut group,
+        "kings-move", CONSTRAINED_SAMPLE_SIZE, &solver);
+    benchmark_solver_constraint::<DefaultAndAdjacentConsecutiveConstraint, _>(
+        &mut group, "adjacent-consecutive", CONSTRAINED_SAMPLE_SIZE, &solver);
 }
 
 fn benchmark_backtracking(c: &mut Criterion) {
